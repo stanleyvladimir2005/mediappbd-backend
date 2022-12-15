ab5f4484cbdd9c83ed0a56d8e2026ebd0e3d4801
Vladimir Stanley Alvarez <stanleyvladimir2005@hotrmail.com>
2022-12-15 12:41:49 -0600

    Eliminacion codigo innecesario

--- src/main/java/com/mitocode/dto/ConsultaDTO.java
+++ src/main/java/com/mitocode/dto/ConsultaDTO.java
@@ -1,34 +1,16 @@
 package com.mitocode.dto;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.hateoas.EntityModel;
 import com.mitocode.model.Medico;
 import com.mitocode.model.Paciente;
 
 //Clase auxiliar para crear el HATEOAS nivel 3 a la consulta
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class ConsultaDTO extends EntityModel<Object> {
 	private Integer idConsulta;
 	private Medico medico;
 	private Paciente paciente;
-
-	public int getIdConsulta() {return idConsulta;}
-
-	public void setIdConsulta(Integer idConsulta) {
-		this.idConsulta = idConsulta;
-	}
-
-	public Medico getMedico() {
-		return medico;
-	}
-
-	public void setMedico(Medico medico) {
-		this.medico = medico;
-	}
-
-	public Paciente getPaciente() {
-		return paciente;
-	}
-
-	public void setPaciente(Paciente paciente) {
-		this.paciente = paciente;
-	}
 }
